95874c478431a0c1bcb99e9c832de896de19bb6e
Danilo Oliveira <danilo.t.a.oliveira@gmail.com>
2021-12-17 22:37:06 -0300

    HomePage and AuthorDao

---
+++ src/main/java/com/blog/bd/ConnectionFactory.java
@@ -0,0 +1,34 @@
+package com.blog.bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+	private String drive = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/blog";	
+	private String user = "root";
+	private String pass = "admin";
+	
+	public Connection getConnection() {
+		Connection conn = null;
+		
+		try {
+			Class.forName(drive);
+			conn  = DriverManager.getConnection(url,user,pass);
+			return conn;
+		} catch (Exception e) {
+			System.out.println(e);
+			return conn;
+		}
+	}
+	
+	public void testConnection() {
+		try {
+			Connection conn = getConnection();
+			System.out.println("Conection Success");
+			System.out.println(conn);
+		} catch (Exception e) {
+			System.out.println("ERROR: "+e);
+		}
+	}
+}
---
+++ src/main/java/com/blog/dao/AuthorDAO.java
@@ -0,0 +1,110 @@
+package com.blog.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import com.blog.bd.ConnectionFactory;
+import com.blog.model.Author;
+
+public class AuthorDAO {
+
+	private Connection conn;
+	
+	public AuthorDAO() {
+		ConnectionFactory cf = new ConnectionFactory();
+		this.conn = cf.getConnection();
+	}
+	
+	public String insert(Author author) {
+		String sql = "INSERT INTO author (name, email, password) VALUES (?,?,?)";		
+		
+		try {			
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, author.getName());
+			stmt.setString(2, author.getEmail());
+			stmt.setString(3, author.getPassword());			
+			
+			int  i = stmt.executeUpdate();
+			stmt.close();
+            
+			if (i > 0) {
+            	return "Success";             
+            }
+                 	  
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+				
+		return "Fail";
+	}
+	
+	public String update(Author author) {
+		String sql = "UPDATE author SET name=?, email=?, password=password WHERE id=?";
+		
+		try {
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, author.getName());
+			stmt.setString(2, author.getEmail());			
+			stmt.setInt(3, author.getId());
+			
+			System.out.println(author.getPassword());
+			int  i = stmt.executeUpdate();
+			stmt.close();
+            
+			if (i > 0) {
+            	return "Success";             
+            }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+				
+		return "Fail";
+	}
+	
+	public String delete(Author author) {
+		String sql = "DELETE from author WHERE id=?";
+		
+		try {
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, author.getId());			
+			
+			int  i = stmt.executeUpdate();
+			stmt.close();
+            
+			if (i > 0) {
+            	return "Success";             
+            }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+				
+		return "Fail";
+	}
+	
+	public ArrayList<Author> getAuthors() {
+		String sql = "SELECT * from author";
+		try {
+			ArrayList<Author> authors = new ArrayList<Author>();
+			PreparedStatement stmt = conn.prepareStatement(sql);			
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			while(rs.next()) {
+				Author author = new Author();
+				author.setId(rs.getInt("id"));
+				author.setName(rs.getString("name"));
+				author.setEmail(rs.getString("email"));
+				authors.add(author);
+			}
+			
+			rs.close();
+			stmt.close();
+			
+			return authors;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}		
+	}
+}
