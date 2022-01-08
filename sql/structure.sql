ad9c313a90151cf9b9067808341eac8791e39ece
DaniloOliveira <danilo.t.a.oliveira@gmail.com>
2022-01-07 21:56:45 -0300

    Update create tables

--- sql/structure.sql
+++ sql/structure.sql
@@ -8,11 +8,22 @@ CREATE TABLE IF NOT EXISTS author (
     password VARCHAR(80) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS category (
+	id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,    
+    description VARCHAR(30)
+);
+
+INSERT INTO category (description) VALUES ('Tecnologia');
+INSERT INTO category (description) VALUES ('Saúde');
+
 CREATE TABLE IF NOT EXISTS post (
 	id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
     author INTEGER,
-    title VARCHAR(20),
-    post TEXT,
-    date DATE,
-    FOREIGN KEY(author) REFERENCES author(id)
+    category INTEGER,
+    title VARCHAR(100),
+    subtitle VARCHAR(100),
+    text TEXT,
+    date TIMESTAMP,
+    FOREIGN KEY(author) REFERENCES author(id),
+    FOREIGN KEY(category) REFERENCES category(id)
 );
